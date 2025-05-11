34c4c8f69a931244b66a06752f9d6ec4f552751d
Olwethu-Nene <230277845@mycput.ac.za>
2025-05-11 18:18:40 +0200

    Review and Category POJOs

--- src/main/java/za/ac/cput/domain/Review.java
+++ src/main/java/za/ac/cput/domain/Review.java
@@ -48,7 +48,6 @@ public class Review {
         this.reviewId = reviewId;
         return this;
     }
-
         public Builder setCustomerId(String customerId) {
             this.customerId = customerId;
             return this;
