139357b563ce2a230beb1cae2581d9e43600572d
Olwethu-Nene <230277845@mycput.ac.za>
2025-05-11 16:33:27 +0200

    Category POJO

--- src/main/java/za/ac/cput/domain/Category.java
+++ src/main/java/za/ac/cput/domain/Category.java
@@ -1,9 +1,33 @@
+/*
+Category.java
+Category POJO class
+Author: Olwethu Nene(230277845)
+Date: 11 May 2025
+
+ */
+
 package za.ac.cput.domain;
 
 public class Category {
 
     private String categoryId;
     private String categoryName;
+
+    public Category() {
+
+    }
+public Category(Builder builder){
+        this.categoryId = builder.categoryId;
+        this.categoryName = builder.categoryName;
+}
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
 
     public static class Builder{
         private String categoryId;
@@ -17,37 +41,20 @@ public class Category {
                 this.categoryName = categoryName;
                 return this;
             }
-            public Builder copy (Category category){
-                this.categoryId = category.categoryId;
-                this.categoryName = category.categoryName;
+        @Override
+        public String toString() {
+            return "Category{" +
+                    "categoryId='" + categoryId + '\'' +
+                    ", categoryName='" + categoryName + '\'' +
+                    '}';
+        }
+            public Builder copy (Builder builder){
+                this.categoryId = builder.categoryId;
+                this.categoryName = builder.categoryName;
                 return this;
             }
-    }
-    public Category(){
 
     }
-    public Category(String categoryId, String categoryName){
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-
-    }
-
-    public String getCategoryId() {
-        return categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "categoryId='" + categoryId + '\'' +
-                ", categoryName='" + categoryName + '\'' +
-                '}';
-    }
-
     public Category build(){
         return new Category();
     }
