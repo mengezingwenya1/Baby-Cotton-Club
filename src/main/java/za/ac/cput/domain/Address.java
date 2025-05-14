b8023f7f023976d3ac44ec79f9c74c0471dbd110
Mengezi Junior Ngwenya <162158030+mengezingwenya1@users.noreply.github.com>
2025-05-14 18:12:47 +0200

    Builder

--- src/main/java/za/ac/cput/domain/Address.java
+++ src/main/java/za/ac/cput/domain/Address.java
@@ -9,17 +9,29 @@ package za.ac.cput.domain;
 
 
 public class Address {
-    private int streetNumber;
-    private String streetName;
-    private String suburb;
-    private String city;
-    private String postalCode;
-    private String province;
+    protected int addressId;
+    protected int streetNumber;
+    protected String streetName;
+    protected String suburb;
+    protected String city;
+    protected String postalCode;
+    protected String province;
 
 
     protected Address (){
 
     }
+    private Address (Builder builder){
+        this.addressId = builder.addressId;
+        this.streetNumber = builder.streetNumber;
+        this.streetName = builder.streetName;
+        this.suburb = builder.suburb;
+        this.city = builder.city;
+        this.postalCode = builder.postalCode;
+        this.province = builder.province;
+
+    }
+    public int getAddressId() { return addressId; }
 
     public int getStreetNumber() {
         return streetNumber;
@@ -56,4 +68,62 @@ public class Address {
                 ", province='" + province + '\'' +
                 '}';
     }
+
+    public static class Builder {
+        private int addressId;
+        private int streetNumber;
+        private String streetName;
+        private String suburb;
+        private String city;
+        private String postalCode;
+        private String province;
+
+        public Builder setAddressId(int addressId) {
+            this.addressId = addressId;
+            return this;
+        }
+
+        public Builder setStreetNumber(int streetNumber) {
+            this.streetNumber = streetNumber;
+            return this;
+        }
+        public Builder setStreetName(String streetName) {
+            this.streetName = streetName;
+            return this;
+        }
+        public Builder setSuburb(String suburb) {
+            this.suburb = suburb;
+            return this;
+        }
+        public Builder setCity(String city) {
+            this.city = city;
+            return this;
+        }
+        public Builder setPostalCode(String postalCode) {
+            this.postalCode = postalCode;
+            return this;
+        }
+        public Builder setProvince(String province) {
+            this.province = province;
+            return this;
+        }
+        public Builder copy(Address address) {
+            this.addressId = address.addressId;
+            this.streetNumber = address.streetNumber;
+            this.streetName = address.streetName;
+            this.suburb = address.suburb;
+            this.city = address.city;
+            this.postalCode = address.postalCode;
+            this.province = address.province;
+            return this;
+        }
+        public Address build (){
+            return new Address (this);
+
+        }
+
+
+    }
+
+
 }
