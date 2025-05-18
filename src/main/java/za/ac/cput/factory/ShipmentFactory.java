a7b224788d55628182f5595d33849460edb2607c
Dairy-Bouy <thapelongwenya700@gmail.com>
2025-05-18 18:23:39 +0200

    Factory and Factory Test for Product and Shipment.

---
+++ src/main/java/za/ac/cput/factory/ShipmentFactory.java
@@ -0,0 +1,18 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Shipment;
+import za.ac.cput.util.Helper;
+
+public class ShipmentFactory {
+public static Shipment createShipment(String shipmentId, String carrierName, String shipmentStatus, double shippingCost) {
+    if (Helper.isNullOrEmpty(shipmentId) || Helper.isNullOrEmpty(carrierName) || Helper.isNullOrEmpty(shipmentStatus)) {
+        return null;
+    }
+    return new Shipment.Builder()
+            .setShipmentId(shipmentId)
+            .setCarrierName(carrierName)
+            .setShipmentStatus(shipmentStatus)
+            .setShippingCost(shippingCost)
+            .build();
+}
+}
