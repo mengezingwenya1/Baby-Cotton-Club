7e5dc2d73e0f54a714ec28d9c9c61e987f5c422e
tsireledzonetshilonwe <165159482+tsireledzonetshilonwe@users.noreply.github.com>
2025-05-18 20:04:37 +0200

    update

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,58 +1,10 @@
 package za.ac.cput.util;
 
-//import org.apache.commons.validator.routines.EmailValidator;
-
-import java.time.LocalDate;
 import org.apache.commons.validator.routines.EmailValidator;
 
 import java.time.LocalDate;
 
 public class Helper {
-
-
-    public static boolean isNullOrEmpty(String s) {
-
-        return s == null || s.isEmpty();
-    }
-
-
-    public static LocalDate getOrderDate(String orderDate) {
-        int year = Integer.parseInt(orderDate.substring(0, 4));
-        int month = Integer.parseInt(orderDate.substring(4, 6));
-        int day = Integer.parseInt(orderDate.substring(6, 8));
-        return LocalDate.of(year, month, day);
-    }
-    public static double calculateSubTotal(int quantity, double unitPrice) {
-        return quantity * unitPrice;
-    }
-//    public static boolean isValidEmail(String email){
-//        EmailValidator validator = EmailValidator.getInstance();
-//        if (validator.isValid(email)) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
-    public static boolean isValidStreetNumber(short streetNumber) {
-        if (streetNumber < 1 || streetNumber > 99999) {
-            return false;
-        }
-        return true;
-    }
-    public static boolean isValidPostalCode(short postalCode) {
-        if (postalCode < 1000 || postalCode > 9999) {
-            return false;
-        }
-        return true;
-
-    }
-
-    public static boolean isValidCategoryId(Integer categoryId) {
-        return categoryId != null && categoryId > 0;
-    }
-
-    public static boolean isValidReviewId(Integer reviewId) {
-        return reviewId != null && reviewId > 0;
 
 
     public static boolean isNullOrEmpty(String s) {
@@ -96,17 +48,10 @@ public class Helper {
         return categoryId != null && categoryId > 0;
     }
 
-    public static boolean isValidAmount(double amount) {
-        return amount > 0;
-    }
     public static boolean isValidReviewId(Integer reviewId) {
         return reviewId != null && reviewId > 0;
     }
 
-    public static boolean isValidDate(LocalDate date) {
-        return date != null && !date.isAfter(LocalDate.now());
-}
-}
     public static boolean isValidAmount(double amount) {
         return amount > 0;
     }
@@ -115,5 +60,3 @@ public class Helper {
         return date != null && !date.isAfter(LocalDate.now());
     }
 }
-
-
